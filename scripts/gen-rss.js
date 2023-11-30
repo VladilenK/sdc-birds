101a16ba7e8c1861ab6705da295f66f754fb7c8e
Vlad Karasov <Vladilen.Karassev@gmail.com>
2023-11-29 21:37:33 -0600

    upd to v0.1

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml'
+    title: 'Птицы Евразии',
+    site_url: 'https://sdc.kz',
+    feed_url: 'https://sdc.kz/feed.xml'
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
